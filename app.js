2dd1a1ae8017f07d61770787c27825ded5e76546
AntonioPizzirusso <71682934+AntonioPizzirusso@users.noreply.github.com>
2021-03-03 23:08:39 -0500

    aergafa

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ const presidents = [
 
 //Challenge Questions
 //1) Filter all presidents, leaving only the Democratic ones
-
+const party = presidents.filter(presidents = (presidents.party ="D"))
+console.table(party);
 //2)Filter all presidents to leave only one term Republican presidents HINT use If statement
 
 //3) return only the last three presidents
